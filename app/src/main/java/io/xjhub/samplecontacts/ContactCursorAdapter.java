9440965583a307b5eb88a1e41ec445c96b0069ff
Ondrej Danko <xjcook@gmail.com>
2016-12-13 00:41:04 +0100

    Fix ContactCursorAdapter

--- app/src/main/java/io/xjhub/samplecontacts/ContactCursorAdapter.java
+++ app/src/main/java/io/xjhub/samplecontacts/ContactCursorAdapter.java
@@ -12,12 +12,6 @@ class ContactCursorAdapter extends CursorAdapter {
 
     private LayoutInflater mInflater;
 
-    // View lookup cache
-    private static class ViewHolder {
-        TextView name;
-        TextView phone;
-    }
-
     ContactCursorAdapter(Context context, Cursor cursor, int flags) {
         super(context, cursor, flags);
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -30,29 +24,15 @@ class ContactCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View convertView, Context context, Cursor cursor) {
-        // Get the data item for this position
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView tvName = (TextView) view.findViewById(R.id.tvName);
+        TextView tvPhone = (TextView) view.findViewById(R.id.tvPhone);
+
         String title = cursor.getString(cursor.getColumnIndex(ContactModel.COLUMN_NAME_TITLE));
         String phone = cursor.getString(cursor.getColumnIndex(ContactModel.COLUMN_NAME_PHONE));
 
-        // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolder viewHolder; // view lookup cache stored in tag
-        if (convertView == null) {
-            // If there's no view to re-use, inflate a brand new view for row
-            viewHolder = new ViewHolder();
-            viewHolder.name = (TextView) convertView.findViewById(R.id.tvName);
-            viewHolder.phone = (TextView) convertView.findViewById(R.id.tvPhone);
-            // Cache the viewHolder object inside the fresh view
-            convertView.setTag(viewHolder);
-        } else {
-            // View is being recycled, retrieve the viewHolder object from tag
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        // Populate the data from the data object via the viewHolder object
-        // into the template view.
-        viewHolder.name.setText(title);
-        viewHolder.phone.setText(phone);
+        tvName.setText(title);
+        tvPhone.setText(phone);
     }
 
 }
