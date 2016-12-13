8794432ebd17128f9d226b362da2cab784e77cc7
Ondrej Danko <xjcook@gmail.com>
2016-12-13 11:21:44 +0100

    Add Order into DbProvider

--- app/src/main/java/io/xjhub/samplecontacts/DbProvider.java
+++ app/src/main/java/io/xjhub/samplecontacts/DbProvider.java
@@ -18,6 +18,8 @@ public class DbProvider extends ContentProvider {
 
     static final int CONTACTS = 1;
     static final int CONTACT_ID = 2;
+    static final int ORDERS = 3;
+    static final int ORDER_ID = 4;
 
     static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
@@ -26,10 +28,12 @@ public class DbProvider extends ContentProvider {
 
     static {
         // multiple rows
-        sUriMatcher.addURI(AUTHORITY, DbModel.Contact.TABLE_NAME, 1);
+        sUriMatcher.addURI(AUTHORITY, DbModel.Contact.TABLE_NAME, CONTACTS);
+        sUriMatcher.addURI(AUTHORITY, DbModel.Order.TABLE_NAME, ORDERS);
 
         // single row
-        sUriMatcher.addURI(AUTHORITY, DbModel.Contact.TABLE_NAME + "/#", 2);
+        sUriMatcher.addURI(AUTHORITY, DbModel.Contact.TABLE_NAME + "/#", CONTACT_ID);
+        sUriMatcher.addURI(AUTHORITY, DbModel.Order.TABLE_NAME + "/#", ORDER_ID);
     }
 
     @Override
@@ -45,8 +49,14 @@ public class DbProvider extends ContentProvider {
             case CONTACTS:
                 return "vnd.android.cursor.dir/vnd." + AUTHORITY + "." + DbModel.Contact.TABLE_NAME;
 
+            case ORDERS:
+                return "vnd.android.cursor.dir/vnd." + AUTHORITY + "." + DbModel.Order.TABLE_NAME;
+
             case CONTACT_ID:
                 return "vnd.android.cursor.item/vnd." + AUTHORITY + "." + DbModel.Contact.TABLE_NAME;
+
+            case ORDER_ID:
+                return "vnd.android.cursor.item/vnd." + AUTHORITY + "." + DbModel.Order.TABLE_NAME;
 
             default:
                 throw new IllegalArgumentException("Unsupported URI: " + uri);
@@ -62,15 +72,31 @@ public class DbProvider extends ContentProvider {
             String[] selectionArgs,
             String sortOrder) {
 
+        String table;
+
         switch (sUriMatcher.match(uri)) {
             case CONTACTS:
+                table = DbModel.Contact.TABLE_NAME;
                 if (TextUtils.isEmpty(sortOrder)) {
                     sortOrder = DbModel.Contact._ID + " ASC";
                 }
                 break;
 
+            case ORDERS:
+                table = DbModel.Order.TABLE_NAME;
+                if (TextUtils.isEmpty(sortOrder)) {
+                    sortOrder = DbModel.Order._ID + " ASC";
+                }
+                break;
+
             case CONTACT_ID:
+                table = DbModel.Contact.TABLE_NAME;
                 selection = selection + DbModel.Contact._ID + " = " + uri.getLastPathSegment();
+                break;
+
+            case ORDER_ID:
+                table = DbModel.Order.TABLE_NAME;
+                selection = selection + DbModel.Order._ID + " = " + uri.getLastPathSegment();
                 break;
 
             default:
@@ -80,7 +106,7 @@ public class DbProvider extends ContentProvider {
         // Make query
         db = mDbHelper.getReadableDatabase();
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-        qb.setTables(DbModel.Contact.TABLE_NAME);
+        qb.setTables(table);
 
         Cursor cursor = qb.query(db,
                 projection,
@@ -89,6 +115,8 @@ public class DbProvider extends ContentProvider {
                 null,
                 null,
                 sortOrder);
+
+        // Register to watch a content URI for changes
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
 
         return cursor;
@@ -98,10 +126,28 @@ public class DbProvider extends ContentProvider {
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
         db = mDbHelper.getWritableDatabase();
-        long rowId = db.insert(DbModel.Contact.TABLE_NAME, null, contentValues);
+        String table;
+        Uri contentUri;
+
+        switch (sUriMatcher.match(uri)) {
+            case CONTACT_ID:
+                table = DbModel.Contact.TABLE_NAME;
+                contentUri = DbModel.Contact.CONTENT_URI;
+                break;
+
+            case ORDER_ID:
+                table = DbModel.Order.TABLE_NAME;
+                contentUri = DbModel.Order.CONTENT_URI;
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        long rowId = db.insert(table, null, contentValues);
 
         if (rowId > 0) {
-            Uri _uri = ContentUris.withAppendedId(uri, rowId);
+            Uri _uri = ContentUris.withAppendedId(contentUri, rowId);
             getContext().getContentResolver().notifyChange(_uri, null);
             return _uri;
         }
@@ -117,20 +163,32 @@ public class DbProvider extends ContentProvider {
             String[] selectionArgs) {
 
         db = mDbHelper.getWritableDatabase();
+        String table;
 
         switch (sUriMatcher.match(uri)) {
             case CONTACTS:
+                table = DbModel.Contact.TABLE_NAME;
+                break;
+
+            case ORDERS:
+                table = DbModel.Order.TABLE_NAME;
                 break;
 
             case CONTACT_ID:
+                table = DbModel.Contact.TABLE_NAME;
                 selection = selection + DbModel.Contact._ID + " = " + uri.getLastPathSegment();
+                break;
+
+            case ORDER_ID:
+                table = DbModel.Order.TABLE_NAME;
+                selection = selection + DbModel.Order._ID + " = " + uri.getLastPathSegment();
                 break;
 
             default:
                 throw new IllegalArgumentException("Unknown URI " + uri);
         }
 
-        int count = db.update(DbModel.Contact.TABLE_NAME, contentValues, selection, selectionArgs);
+        int count = db.update(table, contentValues, selection, selectionArgs);
         getContext().getContentResolver().notifyChange(uri, null);
 
         return count;
@@ -139,20 +197,32 @@ public class DbProvider extends ContentProvider {
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         db = mDbHelper.getWritableDatabase();
+        String table;
 
         switch (sUriMatcher.match(uri)) {
             case CONTACTS:
+                table = DbModel.Contact.TABLE_NAME;
+                break;
+
+            case ORDERS:
+                table = DbModel.Order.TABLE_NAME;
                 break;
 
             case CONTACT_ID:
+                table = DbModel.Contact.TABLE_NAME;
                 selection = selection + DbModel.Contact._ID + " = " + uri.getLastPathSegment();
+                break;
+
+            case ORDER_ID:
+                table = DbModel.Order.TABLE_NAME;
+                selection = selection + DbModel.Order._ID + " = " + uri.getLastPathSegment();
                 break;
 
             default:
                 throw new IllegalArgumentException("Unknown URI " + uri);
         }
 
-        int count = db.delete(DbModel.Contact.TABLE_NAME, selection, selectionArgs);
+        int count = db.delete(table, selection, selectionArgs);
         getContext().getContentResolver().notifyChange(uri, null);
 
         return count;
