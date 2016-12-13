906fe901d7d9d2da4a373dc220bf33e3db2b79f3
Ondrej Danko <xjcook@gmail.com>
2016-12-13 19:52:32 +0100

    Add CreateNewContact AsyncTask Add minimum length form check

--- app/src/main/java/io/xjhub/samplecontacts/NewContactFragment.java
+++ app/src/main/java/io/xjhub/samplecontacts/NewContactFragment.java
@@ -1,15 +1,25 @@
 package io.xjhub.samplecontacts;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewContactFragment extends Fragment {
 
+    private static final String TAG = "NewContactFragment";
     private static final int MIN_LENGTH = 5;
 
     private EditText mNameEditText;
@@ -42,8 +52,56 @@ public class NewContactFragment extends Fragment {
                 if (mPhoneEditText.getText().length() < MIN_LENGTH) {
                     mPhoneEditText.setError(getString(R.string.error_min_length));
                 }
+
+                // Upload new contact
+                String name = mNameEditText.getText().toString();
+                String phone = mPhoneEditText.getText().toString();
+
+                if (name.length() >= MIN_LENGTH && phone.length() >= MIN_LENGTH) {
+                    new CreateNewContact(name, phone).execute();
+                }
             }
         });
 
+    }
+
+    private class CreateNewContact extends AsyncTask<Void, Void, Api.Contact> {
+
+        private Api.Contact mContact;
+
+        CreateNewContact(String name, String phone) {
+            mContact = new Api.Contact(name, phone);
+        }
+
+        @Override
+        protected Api.Contact doInBackground(Void... voids) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(Api.URL + Api.CONTACT_ENDPOINT)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            Api.ContactService service = retrofit.create(Api.ContactService.class);
+            Call<Api.Contact> call = service.createContact(mContact);
+
+            Api.Contact contact = null;
+            try {
+                Log.i(TAG, "Contact uploading...");
+                contact = call.execute().body();
+            } catch (IOException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            }
+
+            return contact;
+        }
+
+        @Override
+        protected void onPostExecute(Api.Contact contact) {
+            if (contact != null) {
+                getActivity().finish();
+            } else {
+                Toast.makeText(getActivity(),
+                        R.string.error_upload_contact, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
