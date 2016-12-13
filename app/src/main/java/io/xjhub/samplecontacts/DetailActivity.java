bef239aa74ef123c2a339c38ee25b66ad2a9d501
Ondrej Danko <xjcook@gmail.com>
2016-12-13 12:49:21 +0100

    Add SimpleCursorAdapter into DetailFragment Change DetailFragment layout to dynamic

--- app/src/main/java/io/xjhub/samplecontacts/DetailActivity.java
+++ app/src/main/java/io/xjhub/samplecontacts/DetailActivity.java
@@ -1,8 +1,6 @@
 package io.xjhub.samplecontacts;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
@@ -13,18 +11,22 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
 
-    @Override
-    public void onAttachFragment(Fragment fragment) {
-        super.onAttachFragment(fragment);
+        // Check that the activity is using the layout version with
+        // the fragment_detail FrameLayout
+        if (findViewById(R.id.fragment_detail) != null) {
+            // Return if restored from a previous state
+            if (savedInstanceState != null) {
+                return;
+            }
 
-        // Pass ContactId into fragment
-        Bundle args = new Bundle();
-        Intent intent = getIntent();
+            // Pass Intent extras into fragment
+            DetailFragment detailFragment = new DetailFragment();
+            detailFragment.setArguments(getIntent().getExtras());
 
-        args.putLong(EXTRA_ID, intent.getLongExtra(EXTRA_ID, -1));
-
-        fragment.setArguments(args);
+            // Add the fragment to the 'fragment_detail' FrameLayout
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_detail, detailFragment).commit();
+        }
     }
 }
--- app/src/main/java/io/xjhub/samplecontacts/DetailFragment.java
+++ app/src/main/java/io/xjhub/samplecontacts/DetailFragment.java
@@ -1,17 +1,66 @@
 package io.xjhub.samplecontacts;
 
 import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
 
-public class DetailFragment extends ListFragment {
+public class DetailFragment extends ListFragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        getLoaderManager().initLoader(3, null, this);
+
+        return rootView;
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = new String[]{
+                DbModel.Order._ID,
+                DbModel.Order.COLUMN_NAME_TITLE
+        };
+
+        String selection = null;
+        String[] selectionArgs = null;
+        Bundle args = getArguments();
+
+        if (args != null) {
+            long id = getArguments().getLong(DetailActivity.EXTRA_ID);
+            if (id != 0) {
+                selection = DbModel.Order._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(id)};
+            }
+        }
+
+        return new CursorLoader(getActivity(),
+                DbModel.Order.CONTENT_URI, projection, selection, selectionArgs, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        cursor.moveToFirst();
+        setListAdapter(new SimpleCursorAdapter(getActivity(),
+                android.R.layout.simple_list_item_1,
+                cursor,
+                new String[]{DbModel.Order.COLUMN_NAME_TITLE},
+                new int[]{android.R.id.text1},
+                0
+        ));
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
