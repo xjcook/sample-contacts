b60905402476671f07f41e353001552da35ec2ac
Ondrej Danko <xjcook@gmail.com>
2016-12-13 15:15:07 +0100

    Add menu resource

--- app/src/main/java/io/xjhub/samplecontacts/DetailActivity.java
+++ app/src/main/java/io/xjhub/samplecontacts/DetailActivity.java
@@ -1,10 +1,13 @@
 package io.xjhub.samplecontacts;
 
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Void> {
@@ -40,18 +43,38 @@ public class DetailActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem newContactItem = menu.findItem(R.id.action_newcontact);
+
+        newContactItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent intent = new Intent(DetailActivity.this, NewContactActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
     public Loader<Void> onCreateLoader(int i, Bundle bundle) {
         long contactId = getIntent().getLongExtra(EXTRA_ID, 0);
         return new OrderLoader(this, contactId);
     }
 
     @Override
-    public void onLoadFinished(Loader<Void> loader, Void aVoid) {
-
-    }
+    public void onLoadFinished(Loader<Void> loader, Void aVoid) {}
 
     @Override
-    public void onLoaderReset(Loader<Void> loader) {
+    public void onLoaderReset(Loader<Void> loader) {}
 
-    }
 }
--- app/src/main/java/io/xjhub/samplecontacts/MainActivity.java
+++ app/src/main/java/io/xjhub/samplecontacts/MainActivity.java
@@ -1,10 +1,13 @@
 package io.xjhub.samplecontacts;
 
 import android.app.LoaderManager;
+import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Void> {
@@ -21,6 +24,29 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem newContactItem = menu.findItem(R.id.action_newcontact);
+
+        newContactItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent intent = new Intent(MainActivity.this, NewContactActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
     public Loader<Void> onCreateLoader(int i, Bundle bundle) {
         return new ContactLoader(this);
     }
@@ -30,4 +56,5 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onLoaderReset(Loader<Void> loader) {}
+    
 }
